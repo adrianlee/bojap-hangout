676ffe20682c991551454aa0385c8612c3c7008f
Adrian Lee <jun.irok@gmail.com>
2014-01-10 02:32:08 -0500

    Undo changes til Bootstrap on single app.js

--- static/js/bojap.js
+++ static/js/bojap.js
@@ -17,6 +17,39 @@ angular.module('app', ['ngRoute'])
 
 .controller('MainCtrl', function ($scope) {
   $scope.bojap = window.bojap;
+
+  $(function () {
+      gapi.hangout.render('start_hangout', {
+          'render': 'createhangout',
+          'topic': "Adrian's Room",
+          'hangout_type': 'normal',
+          'initial_apps': [],
+          'widget_size': 175
+      });
+  });
+
+  fetchRooms();
+  setInterval(fetchRooms, 10000);
+
+  function fetchRooms() {
+      $.get("/rooms", function (data) {
+          // console.log("rooms online: " + data.length);
+          var list = document.getElementById("list_rooms");
+          var html = "";
+
+          for (var i in data) {
+              html += "<tr>";
+              html += "<td>" + data[i].local_participant.person.displayName + "</td>";
+              html += "<td>" + data[i].topic + "</td>";
+              html += "<td>" + data[i].participants.length + "</td>";
+              html += "<td>" + "online" + "</td>";
+              html += "<td>" + "<a href='" + data[i].url + "' target='_blank'>join</a>" + "</td>";
+              html += "</tr>";
+          }
+
+          list.innerHTML = html;
+      });
+  }
 })
 
 .controller('AppCtrl', function ($scope) {
